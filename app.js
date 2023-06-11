09482e11bfa87042492d0f593a680853b3fc00b0
restindevlops <124300271+restindevlops@users.noreply.github.com>
2023-06-11 22:38:23 +0530

    Routers and Filters (Task03)

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 
 const express = require('express');
+const bodyParser=require('body-parser');
 
 const app= express();
 
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
 app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next(); // Allows the request to continue to the next middleware
-})
-app.use((req,res,next)=>{
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express.js</h1>');
+    res.status(404).send('<h1> Page not found </h1>');
 })
 
 app.listen(3000);
