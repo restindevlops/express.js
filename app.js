f692463fe47731d525752317cffc4840c8e276fd
restindevlops <124300271+restindevlops@users.noreply.github.com>
2023-06-12 23:59:22 +0530

    Serving HTML , CSS and JS (VIEWS) (Task 05)

--- app.js
+++ app.js
@@ -1,63 +1,21 @@
+const path=require('path');
 
 const express = require('express');
-
-const fs=require('fs');
-
 const bodyParser=require('body-parser');
 
 const app= express();
 
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/" , (req,res,next)=>{
-    fs.readFile('username.txt',(err,data)=>{
-        if(err){
-            console.log(err);
-            data="No chat exists"
-        }
-        res.send(`${data} <form 
-        action="/" method="POST"
-         onsubmit="document.getElementById('username').value= localStorage.getItem('username')"> 
-         <input type="text" id="message" name="message" placeholder="Enter your message">
-         <input type="hidden" id="username" name="username">
-         <br>
-         <button type="submit">Send</button>
-         </form>`);
-    })
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-  
-   
-});
-
-app.post("/", (req,res,next)=>{
-
-    console.log(req.body.username);
-    console.log(req.body.message);
-
-    if(req.body.message!=undefined){
-        fs.writeFile( 'username.txt', `<br> ${req.body.username}: ${req.body.message}`,{flag:'a'},(err)=>
-        err? console.log(err): res.redirect("/"));
-    }
-    else{
-        fs.writeFile('username.txt', `<br> ${req.body.username} is online`,{flag:'a'},(err)=>
-        err? console.log(err): res.redirect("/"));
-    }
-   
-
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views', '404.html'))
 })
-
-app.get("/login" , (req,res,next)=>{
-    res.send(`<form 
-     action="/" method="POST"
-     onsubmit="localStorage.setItem('username', document.getElementById('username').value)"> 
-     <input type="text" id="username" name="username" placeholder="Enter your username">
-     <br>
-     <button type="submit">Login</button>
-     </form>`);
-    
- });
-
-
-
 
 app.listen(3000);
