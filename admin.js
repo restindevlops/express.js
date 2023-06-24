a5721c35159c6fa577d4c127f54113732f4e2e8f
restindevlops <124300271+restindevlops@users.noreply.github.com>
2023-06-24 22:30:17 +0530

    Dynamic Routes 01 Task 08

--- admin.js
+++ admin.js
@@ -1,13 +1,18 @@
-const path=require('path');
+const path = require('path');
 
-const express=require('express');
+const express = require('express');
 
-const router=express.Router();
+const adminController = require('../controllers/admin');
 
-const productController=require('../controller/products');
+const router = express.Router();
 
-router.get('/add-product' ,productController.getAddProducts);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product',productController.postAddProducts);
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-module.exports=router;
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
+module.exports = router;
--- shop.js
+++ shop.js
@@ -1,12 +1,21 @@
+const path = require('path');
 
-const path=require('path');
+const express = require('express');
 
-const express=require('express');
+const shopController = require('../controllers/shop');
 
-const router=express.Router();
+const router = express.Router();
 
-router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
-})
+router.get('/', shopController.getIndex);
 
-module.exports=router;
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
